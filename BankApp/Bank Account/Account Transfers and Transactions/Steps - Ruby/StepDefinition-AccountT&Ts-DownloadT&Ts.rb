541940c1c8e44d138ae9d1b370ff6f121e9ff7a4
TheMightyLlama <TheMightyLlama@users.noreply.github.com>
2017-01-31 10:07:47 +0000

    Create StepDefinition-AccountT&Ts-DownloadT&Ts.rb

---
+++ BankApp/Bank Account/Account Transfers and Transactions/Steps - Ruby/StepDefinition-AccountT&Ts-DownloadT&Ts.rb
@@ -0,0 +1,32 @@
+Given /^the following user:$/ do 
+    # do something
+end
+
+Given /^I am logged in as Bob$/ do 
+    # do something
+end
+
+When /^I want to download transactions or transfers against my account$/ do 
+    # do something
+end
+
+Then /^I am not able to perform a download$/ do 
+    # do something
+end
+
+Then /^I should be able to start a download$/ do 
+    # do something
+end
+
+And /^I have no transactions or transfers against my account$/ do 
+    # do something
+end
+
+And /^I have transactions or transfers against my account$/ do 
+    # do something
+end
+
+And /^I should download a csv file of my transactions and transfers$/ do 
+    # do something
+end
+
